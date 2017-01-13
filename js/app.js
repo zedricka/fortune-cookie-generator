1b80253e39edf0f83d5eb9438c92da35ecfcb105
zedricka <zedrick.ara@gmail.com>
2017-01-13 09:22:34 -1000

    wow

--- js/app.js
+++ js/app.js
@@ -1,18 +1,12 @@
-// My Array
-var fortunesList = ["Incredible", "Amazing", "Wow", "Bleh"];
-
-//Randomize
-for(var rawr=0; rawr < 4; rawr++){
-  console.log (fortunesList[rawr]);
+function generateFortuneCookie () {
+  var fortunesList = ["Incredible", "Amazing", "Wow", "Bleh"];
+  var node = document.createElement("LI");                 // Create a <li> node
+  var textnode = document.createTextNode(fortunesList[((Math.random() * 10) + 0)];         // Create a text node
+  node.appendChild(textnode);                              // Append the text to <li>
+  document.getElementById("fortune-cookie-text").appendChild(node);     // Append <li> to <ul> with id="myList"
 }
 
-var random = fortunesList.splice[Math.floor(Math.random() * fortunesList.length)];
-
-function generateFortuneCookie () {
-
-} 
-// Appending fortune-cookie-text as li
-document.getElementById("fortune-cookie-text")
 
 // https://zenscript.wordpress.com/2013/11/23/how-to-pick-a-random-entry-out-of-an-array-javascript/
 // http://stackoverflow.com/questions/13484322/get-random-item-from-array-within-a-for-loop-then-remove-from-array
+// http://www.w3schools.com/js/js_htmldom_nodes.asp
